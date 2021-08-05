063ae962a09a5e66bbfff4586518ffe0fb27f8f0
Anatoly <devilfoxok@gmail.com>
2021-08-05 12:51:35 +0800

    Решение 3 модуль 1-5 задачи

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
   // ваш код...
+  return camelized = str.split('-')
+                        .map((item, index) => index > 0 ? item[0].toUpperCase() + item.slice(1) : item)
+                        .join('');
 }
+
+
