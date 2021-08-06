4807f51ba4c64f33b5a4cfafdb61836b00617d52
Anatoly <devilfoxok@gmail.com>
2021-08-06 19:28:43 +0800

    Решение 4 модуль 1-3 задачи

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let friendsCon = friends.map(item => item.firstName + " " + item.lastName);
+
+  let ul = document.createElement('ul');
+  
+  for (let key of friendsCon) {
+    let li = document.createElement('li');
+    li.innerHTML = key;
+    ul.append(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,25 @@
 function highlight(table) {
   // ваш код...
+  for ( let i = 1; i < table.rows.length; i++ ) {
+    let row = table.rows[i];
+    
+    if ( row.cells[3].dataset.available == 'true' ) {
+      row.classList.add('available');
+    } else if ( row.cells[3].dataset.available == 'false' ) {
+      row.classList.add('unavailable');
+    } else {
+      row.setAttribute('hidden', true);
+    }
+
+    if ( row.cells[2].innerHTML == 'm' ) {
+      row.classList.add('male');
+    } else if ( row.cells[2].innerHTML == 'f' ) {
+      row.classList.add('female');
+    }
+
+    if ( +row.cells[1].innerHTML < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  }
+  
 }
